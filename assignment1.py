f5e6c27d745dccaf222e089e5faff3ad375f0371
Vinayak Agarwal <vinayak.agarwal@iiitb.org>
2020-08-14 21:42:11 +0530

    multiplication function added

--- assignment1.py
+++ assignment1.py
@@ -4,12 +4,15 @@ def addition(a,b):
 def subtraction(a,b):
 	return a-b;
 
+def multiplication(a,b):
+	return a*b
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
+	print(multiplication(a,b))
 
 
 '''
